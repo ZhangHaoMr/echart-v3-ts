5bac6d045a0f477968e86033dea14a33e102b300
ZhangHao <3557683235@qq.com>
2023-05-29 21:12:35 +0800

    feat(seller): 实现 商家销售统计 echarts 图表

--- src/components/Seller.vue
+++ src/components/Seller.vue
@@ -11,9 +11,21 @@ import { getSeller } from '@/api/seller'
 const { proxy } = getCurrentInstance()
 
 // 保存echarts实例
-const echart = ref(null)
+const echart = ref()
+// 保存所有数据
+const resAllData = ref<any>()
 
-const resData = ref<any>()
+// 页码
+const currentPage = ref(1)
+// 条数
+const pageSize = ref(5)
+// 总条数
+const totalPage = ref(0)
+// 每页保存的数据
+const showData = ref([])
+
+// 保存定时器
+let timerId = ref<number>()
 
 onMounted(() => {
   initCharts()
@@ -22,15 +34,33 @@ onMounted(() => {
 // 初始化echarts
 const initCharts = () => {
   echart.value = proxy.$echarts.init(document.getElementById('charts'))
-  console.log('echart.value', echart.value)
+  // console.log('echart.value', echart.value)
+
+  echart.value.on('mouseover', () => {
+    clearInterval(timerId.value)
+  })
+  echart.value.on('mouseout', () => {
+    startInterval()
+  })
 }
 
 // 获取后台返回的图表数据
 const getData = async () => {
   try {
     const res = await getSeller()
-    resData.value = res
+    resAllData.value = res
+    // 从小到大排序
+    resAllData.value.sort((a: any, b: any) => {
+      return b.value - a.value
+    })
+
+    totalPage.value =
+      resAllData.value.length % 5 === 0
+        ? resAllData.value.length / 5
+        : resAllData.value.length / 5 + 1
+
     updataChart()
+    startInterval()
   } catch (e) {
     console.log(e)
   }
@@ -38,7 +68,47 @@ const getData = async () => {
 
 // 配置图标 并 配置
 const updataChart = () => {
-  
+  // 分页获取数据
+  const stant = (currentPage.value - 1) * pageSize.value
+  const end = pageSize.value * currentPage.value
+  showData.value = resAllData.value.slice(stant, end)
+
+  // 获取所有name数据
+  const nameData = showData.value.map((item: any) => item.name)
+  // 获取所有value数据
+  const valueData = showData.value.map((item: any) => item.value)
+  // 图标配置
+  const option = {
+    xAxis: {
+      type: 'value'
+    },
+    yAxis: {
+      type: 'category',
+      data: nameData
+    },
+    series: [
+      {
+        type: 'bar',
+        data: valueData
+      }
+    ]
+  }
+  echart.value.setOption(option)
+}
+
+// 动态加载分页数据
+const startInterval = () => {
+  if (timerId.value) {
+    clearInterval(timerId.value)
+  }
+  timerId.value = setInterval(() => {
+    currentPage.value++
+
+    if (totalPage.value === currentPage.value) {
+      currentPage.value = 1
+    }
+    updataChart()
+  }, 3000)
 }
 </script>
 
